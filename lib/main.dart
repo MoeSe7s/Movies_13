d4231aec62cf67918252b7fcd4c77fa962850651
moh66 <moh.6666722@gmail.com>
2025-03-11 09:26:43 +0200

    Movies newLin + file

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:movies_project/forget_password/forget_password.dart';
+import 'package:movies_project/login_screen/login_screen.dart';
 import 'package:movies_project/on_boarding/on_boarding.dart';
 import 'package:provider/provider.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'provider/provider.dart';
-import 'screens/home_screen/home_screen.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (context)=>LocalProvider(),
-      child: MyApp()));
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => LocalProvider(),
+      child: MyApp(),
+    ),
+  );
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: Locale(context.watch<LocalProvider>().locale),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       initialRoute: Onboarding.routeName,
+      debugShowCheckedModeBanner: false,
       routes: {
-        Onboarding.routeName:(context)=> Onboarding(),
-        HomeScreen.routeName:(context)=> HomeScreen()
+        LoginScreen.routeName: (context) => LoginScreen(
+          changeLanguage: (languageCode) {
+            context.read<LocalProvider>().changeLanguage(languageCode);
+          },
+        ),
+        Onboarding.routeName: (context) => const Onboarding(),
+        ForgetPasswordScreen.routeName: (context) =>
+        const ForgetPasswordScreen(),
       },
     );
   }
